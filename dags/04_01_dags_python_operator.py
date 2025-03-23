359d0c13e3695603a5b63345c3bfbbe2e2f84580
herzblue <herzblue@gmail.com>
2025-03-23 17:38:21 +0900

    class4-1 fix name

--- dags/04_01_dags_python_operator.py
+++ dags/04_01_dags_python_operator.py
@@ -3,6 +3,7 @@ import pendulum
 import random
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
 
 with DAG(
     dag_id = 'dags_python_operator',
@@ -11,14 +12,7 @@ with DAG(
     catchup = False
 ) as dag:
     
-    def select_fruit():
-        fruit = ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
-        rand_int = random.randint(0, 3) # 0~3 까지 int 값 중 1개 랜덤 생성
-        print(fruit[rand_int]) # fruit[0|1|2|3] -> APPLE|BANANA|ORANGE|AVOCADO
-        
-    py_t1 = PythonOperator(
-        task_id = 'py_t1',
-        python_callable = select_fruit # 어떤 함수를 실행시킬 것인지 지정
+    task_get_sftp = PythonOperator(
+        task_id = 'task_get_sftp',
+        python_callable = get_sftp
     )
-    
-    py_t1
