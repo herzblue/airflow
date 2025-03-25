e03074357b6f0e5caccdfb1f74eb32119b4a9e13
herzblue <herzblue@gmail.com>
2025-03-26 00:02:22 +0900

    edit dag id

--- dags/04_06_dags_python_with_op_kwrags.py
+++ dags/04_06_dags_python_with_op_kwrags.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import regist2
 
 with DAG(
-    dag_id="dags_python_with_op_args",
+    dag_id="dags_python_with_op_kwrags",
     schedule="30 6 * * *",  # 각 자리 = 분, 시, 일, 월, 요일
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
