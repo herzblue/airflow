28fc9bd0d609896c21a1baa361cf346c41481294
herzblue <herzblue@gmail.com>
2025-03-25 23:44:01 +0900

    class4-5

---
+++ dags/04_05_dags_python_with_op_args.py
@@ -0,0 +1,19 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",  # 매일 06시 30분
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    regist_t1 = PythonOperator(
+        task_id="regist_t1",
+        python_callable=regist,
+        op_arigs=["herzblue", "man", "kr", "seoul"],
+    )
+
+    regist_t1
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,5 +1,12 @@
 def get_sftp():
-    print('sftp 작업을 시작합니다.')
+    print("sftp 작업을 시작합니다.")
+
+
+def regist(name, sex, *args):
+    print(f"이름: {name}")
+    print(f"성별: {sex}")
+    print(f"기타옵션들: {args}")
+
 
 # if __name__ == '__main__':
 #     get_sftp()
