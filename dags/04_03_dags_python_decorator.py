83018d1a7bb85b1059aa2658f3bf4261ea24e276
herzblue <herzblue@gmail.com>
2025-03-25 23:43:53 +0900

    linter

--- dags/04_03_dags_python_decorator.py
+++ dags/04_03_dags_python_decorator.py
@@ -1,18 +1,16 @@
-import datetime
 import pendulum
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.python import PythonOperator
 
 with DAG(
-    dag_id ="dags_python_task_decorator",
-    schedule = '0 2 * * 1', # 매일 06시 30분
-    start_date = pendulum.datetime(2023, 3, 1, tz = 'Asia/Seoul'),
-    catchup = False,
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",  # 매일 06시 30분
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
 ) as dag:
-    
-    @task(task_id = "python_task_1")
+
+    @task(task_id="python_task_1")
     def print_context(some_input):
         print(f"some_input: {some_input}")
-    
-    python_task_1 = print_context(some_input = 'task decorator 실행')
+
+    python_task_1 = print_context(some_input="task decorator 실행")
